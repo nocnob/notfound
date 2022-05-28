edca61f25e51087c7fab6d766d067f7da1e9b0c7
likui <likui@notfound.cn>
2022-05-29 00:51:07 +0800

    add prismjs

--- src/pages/posts/{asciidoc.pageAttributes__slug}.tsx
+++ src/pages/posts/{asciidoc.pageAttributes__slug}.tsx
@@ -1,11 +1,28 @@
 import { graphql, PageProps } from "gatsby";
+import Prism from "prismjs";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-ruby";
+import "prismjs/themes/prism.css";
 import React from "react";
-
 import "./asciidoctor.css";
 
 function Template({ data }: PageProps<Queries.PostQueryQuery>) {
   const post = data.asciidoc;
-  return <div dangerouslySetInnerHTML={{ __html: post?.html || "" }} />;
+  const [html, setHtml] = React.useState("");
+
+  React.useEffect(() => {
+    if (post?.html) {
+      const doc = new DOMParser().parseFromString(post.html, "text/html");
+      doc.querySelectorAll("pre.highlight > code").forEach(function (el) {
+        Prism.highlightElement(el, false);
+      });
+      setHtml(doc.body.innerHTML);
+    } else {
+      setHtml("");
+    }
+  }, [post?.html]);
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
 
 export default Template;
